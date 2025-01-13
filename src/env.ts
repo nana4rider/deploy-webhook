839c9c1e3f1f62ef47329568d69ab065038e5a05
Shunichiro Aki <register.develop@nana4.net>
2025-01-14 04:11:08 +0900

    feat: desc

--- src/env.ts
+++ src/env.ts
@@ -3,13 +3,15 @@ import { cleanEnv, num, port, str } from "envalid";
 const env = cleanEnv(process.env, {
   LOG_LEVEL: str({ default: "info", desc: "ログ出力" }),
   PORT: port({
-    default: 3000,
     desc: "HTTPサーバーのポート",
+    default: 3000,
   }),
   DEPLOY_SCRIPT_PATH: str({
+    desc: "デプロイスクリプトの場所",
     example: "/path/to/deploy.sh",
   }),
   DISCORD_WEBHOOK_URL: str({
+    desc: "Discord WebhookのURL",
     example: "https://discord.com/api/webhooks/.../...",
   }),
   WEBHOOK_SECRET: str({}),
